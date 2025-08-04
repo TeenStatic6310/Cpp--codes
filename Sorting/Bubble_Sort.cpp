1fdc720fc4246d25d586c09591285eecc0a01f6d
Raghav Pathak <raghavpathak6310@gmail.com>
2025-08-04 18:06:19 +0530

    Update Bubble_Sort.cpp

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -1,15 +1,49 @@
-#include<iostream>
+// Code 
+
+#include <iostream>
 using namespace std;
 
-int BubbleSort(int arr[], int size){
-  for(int i = size - 1; i > 0; i --){
-      for(int j = 0; j < size - 1; j ++){
-          
-      }
-  }
+void BubbleSort(int arr[], int size) {
+    for (int i = size - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {  
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    for (int k = 0; k < size; k++) {
+        cout << arr[k] << " ";
+    }
+    cout << endl;
 }
 
-int main(){
-int size;
+int main() {
+    int size;
+    cin >> size;
 
+    int nums[5] = {10, 1, 7, 6, 9};  
+
+    BubbleSort(nums, 5);  
+    return 0;
 }
+
+
+// Code the Leetcode : 
+
+/*
+#include <bits/stdc++.h> 
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    cout << endl;
+}
+*/
